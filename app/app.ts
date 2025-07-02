340ae5372e1c8e83f210ea0dd5add2c1715bfb7c
Arfan Ahmed <arfan18@cse.pstu.ac.bd>
2025-07-02 15:29:42 +0600

    add: router in app.ts

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import contactRouter from './modules/contact/contact.route';
 
-const app = express()
+const app = express();
+app.use(express.json());
 dotenv.config();
+
+app.use('api/contact', contactRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
